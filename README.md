c82df56bdc639780bfbe26c06a58f68a5c35b02f
David Nascimento <david@thecreativefarm.pt>
2024-07-17 15:04:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# WooCommerce Gutenberg Checkout VAT field for Moloni
+
+Adds a Moloni-compatible VAT field to the WooCommerce Checkout Gutenberg billing fields block, since their own plugin doesn't support the new WordPress editor.
+
+## Usage
+
+Either upload the plugin to the wp-content/plugins directory, or add it via the Plugins - Add Plugin menu in the WordPress admin.
+
+## Development
+
+Open any text editor and have fun coding!
+
+## Support
+
+Feel free to email us at [support@thecreativefarm.pt](mailto:support@thecreativefarm.pt) if you have any questions or need help, but understand that we might not be able to help you with more complex WooCommerce integration issues.
+
+## License
+
+This very very simple WordPress plugin is licensed under the MIT License. You are free to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of this software, subject to the inclusion of the original copyright notice and this permission notice in all copies or substantial portions of the software. Also, you cannot hold us liable for anything.
---
+++ tcf-moloni-gutenberg-vat.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: WooCommerce Gutenberg Checkout VAT field for Moloni
+Description: Adds a Moloni-compatible VAT field to WooCommerce Checkout Gutenberg billing fields block. Validation is done only for Portugese VATs.
+Version: 1.0
+Author: The Creative Farm
+*/
+
+add_action("plugins_loaded", "define_vat_field_constant", 1);
+
+function define_vat_field_constant()
+{
+    if (!defined("VAT_FIELD")) {
+        define("VAT_FIELD", "_billing_vat");
+    }
+}
+
+add_action("woocommerce_init", function () {
+    woocommerce_register_additional_checkout_field([
+        "id" => "tcf-moloni-gutenberg-vat/vat-number",
+        "label" => __("NIF", "tcf-moloni-gutenberg-vat"),
+        "location" => "address",
+        "required" => false,
+        "attributes" => [
+            "autocomplete" => "vat-number",
+            "pattern" => "[0-9]{9}",
+            "title" => __("Número Contribuinte", "tcf-moloni-gutenberg-vat"),
+        ],
+    ]);
+
+    add_action(
+        "woocommerce_validate_additional_field",
+        function (WP_Error $errors, $field_key, $field_value) {
+            if ("tcf-moloni-gutenberg-vat/vat-number" === $field_key) {
+                $match = preg_match('/^[0-9]{9}$/', $field_value);
+
+                if (0 === $match || false === $match) {
+                    $errors->add(
+                        "invalid_vat_number",
+                        __(
+                            "Por favor introduza um NIF válido.",
+                            "tcf-moloni-gutenberg-vat"
+                        )
+                    );
+                }
+            }
+            return $error;
+        },
+        10,
+        3
+    );
+});
+
+add_action(
+    "woocommerce_set_additional_field_value",
+    function ($key, $value, $group, $wc_object) {
+        if ("tcf-moloni-gutenberg-vat/vat-number" !== $key) {
+            return;
+        }
+
+        if ("billing" === $group) {
+            $billing_vat = "_billing_vat";
+        } else {
+            $billing_vat = "_billing_vat";
+        }
+
+        $wc_object->update_meta_data($billing_vat, $value, true);
+    },
+    10,
+    4
+);
